471f48ba73e54fd09c0306a807c6928695ab5151
Callegari <Fabio.Callegari@cbre.com>
2020-10-14 10:21:49 -0700

    add theme-ui

--- pages/notes/[id].jsx
+++ pages/notes/[id].jsx
@@ -1,20 +1,14 @@
-import React from "react";
-import { useRouter } from "next/router";
-import Link from "next/link";
+import { jsx } from 'theme-ui'
+import { useRouter } from 'next/router'
+import Link from 'next/link'
 
 export default () => {
-  //functional component, the whole component is the render function.
-  const router = useRouter();
-  // id is the name of the file [id]. You can put anything.
-  const { id } = router.query;
+  const router = useRouter()
+  const { id }= router.query
 
   return (
-    <div>
+    <div sx={{variant: 'containers.page'}}>
       <h1>Note: {id} </h1>
-
-      <Link href="/notes">
-        <a>Notes</a>
-      </Link>
     </div>
-  );
-};
+  )
+}
