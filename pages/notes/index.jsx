09c2ef5106a9aa6166212b260578dbac6f081ad5
Callegari <Fabio.Callegari@cbre.com>
2020-10-15 10:23:59 -0700

    setup env variables for deployment

--- pages/notes/index.jsx
+++ pages/notes/index.jsx
@@ -3,14 +3,15 @@ import { jsx } from 'theme-ui'
 import Link from 'next/link'
 
 export default ({notes}) => {
+
   return (
     <div sx={{variant: 'containers.page'}}>
       <h1>My Notes</h1>
 
       <div sx={{display: 'flex', justifyContent: 'space-between', alignItems: 'center', flexWrap: 'wrap'}}>
         {notes.map(note => (
-          <div sx={{width: '33%', p: 2}} key={note.id}>
-            <Link href="/notes/[id]" as={`/notes/${note.id}`}>
+          <div sx={{width: '33%', p: 2}}>
+            <Link key={note.id} href="/notes/[id]" as={`/notes/${note.id}`}>
               <a sx={{textDecoration: 'none', cursor: 'pointer'}}>
                 <div sx={{variant: 'containers.card',}}>
                   <strong>{note.title}</strong>
@@ -24,11 +25,9 @@ export default ({notes}) => {
   )
 }
 
-
 export async function getServerSideProps() {
-  const res = await fetch(`http://localhost:3000/api/note`)
+  const res = await fetch(`${process.env.API_URL}/api/note/`)
   const {data} = await res.json()
-
   return {
     props: {notes: data}
   }
